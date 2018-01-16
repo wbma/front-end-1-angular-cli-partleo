cc7f8bb3149b01bd9ef0f4dee745a69ebfd63bdc
Leo Partanen <leo.partanen@metropolia.fi>
2018-01-16 13:12:25 +0200

    'message'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MainComponent } from './main/main.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    MainComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
